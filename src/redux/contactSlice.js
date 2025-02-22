ba03994e1143c4b1a751b06642f463b58623bc46
Danik2023 <danik.kyrteu2@gmail.com>
2025-02-22 19:15:58 +0200

    ...

---
+++ src/redux/contactSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { nanoid } from 'nanoid';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: { contacts: [] },
+  reducers: {
+    addContact: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(name, number) {
+        return {
+          payload: {
+            id: nanoid(),
+            name,
+            number,
+          },
+        };
+      },
+    },
+    deleteContact(state, action) {
+      state.contacts = state.contacts.filter(
+        contact => contact.id !== action.payload
+      );
+    },
+  },
+});
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,6 @@
-import { createStore } from 'redux';
-import { devToolsEnhancer } from '@redux-devtools/extension';
-
-const initialState = {
-  account: {
-    balance: 0,
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
   },
-};
-
-const rootReducer = (state = initialState, action) => {
-  return state;
-};
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+});
