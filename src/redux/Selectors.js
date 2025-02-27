ef5ae89819f1302f386c32380c8caf2363fdcc8f
Danik2023 <danik.kyrteu2@gmail.com>
2025-02-27 19:51:00 +0200

    ...

--- src/redux/Selectors.js
+++
@@ -1,9 +0,0 @@
-// import { useDispatch, useSelector } from 'react-redux';
-
-// export const useContacts = () => {
-//   const contacts = useSelector(state => state.contacts.contacts);
-//   const filter = useSelector(state => state.contacts.filter || '');
-//   const dispatch = useDispatch();
-
-//   return { contacts, filter, dispatch };
-// };
